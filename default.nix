9f1f0fb0eebcae1b185de9f394edffa8001ef3b5
tuxa <69870860+mrtuxa@users.noreply.github.com>
2023-02-06 10:03:58 +0100

    Update default.nix (#7)

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/sozi-projects/Sozi/releases/download/v22.03.25/sozi_22.3.25-1648241936_amd64.deb";
-    sha256 = "1zv48m8g7y6f40y9zp1vcyjg6z8bdy193945kif5c6d5kfzgvvxy";
+    sha256 = "sha256-7o4+C+YgRbEZGqx6Ul65VktcrW3VyXRO1eKr1RHGONU=";
   };
 
   installPhase = ''
